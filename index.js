8d66ad8b1c9a1baf46f57c58309ebe2e0d0d009e
vimestre <vimestre@canamashnos.com>
2023-06-20 10:32:15 +0200

    añadida llamada poligonos y parcelas

--- index.js
+++ index.js
@@ -13,6 +13,59 @@ app.use(cors());
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
 
+
+
+
+app.get('/Parcelas/:ProvinciaId/:MunicipioId/:PoligonoId', (async (req, res) => {
+    const MunicipioId = req.params.MunicipioId;
+    const ProvinciaId = req.params.ProvinciaId;
+    const Poligono = req.params.PoligonoId;
+    const urlSigPac = `https://sigpac.mapama.gob.es/fega/serviciosvisorsigpac/query/parcelas/${ProvinciaId}/${MunicipioId}/0/0/${Poligono}.geojson`
+
+    try {
+        const resp = await fetch(urlSigPac, {
+            method: 'GET'
+        });
+
+        const data = (await resp.json()).features;
+        const result = [];
+        for (var i = 0; i < data.length; i++) {
+            result.push(data[i].properties)
+        }
+        res.send(JSON.stringify(result));
+
+    } catch (err) {
+        console.log(err)
+        res.send('ERROR');
+    }
+}));
+
+app.get('/Poligonos/:ProvinciaId/:MunicipioId', (async (req, res) => {
+    const MunicipioId = req.params.MunicipioId;
+    const ProvinciaId = req.params.ProvinciaId;
+    const urlSigPac = `https://sigpac.mapama.gob.es/fega/serviciosvisorsigpac/query/poligonos/${ProvinciaId}/${MunicipioId}/0/0.geojson`
+
+    try {
+        const resp = await fetch(urlSigPac, {
+            method: 'GET'
+        });
+
+        const data = (await resp.json()).features;
+        const result = [];
+        for (var i = 0; i < data.length; i++) {
+            result.push(data[i].properties)
+        }
+
+        res.send(JSON.stringify(result));
+
+    } catch (err) {
+        console.log(err)
+        res.send('ERROR');
+    }
+
+
+}));
+
 app.get('/Regepa/:NIF', (async (req, res) => {
 
     const urlRegepa = 'https://www.mapa.gob.es/app/regepa/ResBusCon.aspx?id=es'
@@ -23,33 +76,32 @@ app.get('/Regepa/:NIF', (async (req, res) => {
     body.append('TxtDNI', DNI)
     body.append('bt_matr1', 'Buscar')
 
-    const resp = await fetch(urlRegepa, {
-        method: 'POST',
-        body: body
-    });
-
-    const data = await resp.text();
-
     try {
+
+        const resp = await fetch(urlRegepa, {
+            method: 'POST',
+            body: body
+        });
+
+        const data = await resp.text();
+
         let start = data.search(`<td headers="ID0EDBA1" class="colu1_tabla3" style="border-left-width:0px;">`);
         if (start > 0) {
             let sub = data.substring(start, start + 200)
             start = sub.search(`<span class="tabla_texto_normal" style="float:left;">`) + `<span class="tabla_texto_normal" style="float:left;">`.length;
-            console.log(start + " -> " + sub);
-            console.log(sub.substring(start, start + 10));
             res.send(sub.substring(start, start + 10))
         } else {
-            console.log('No encontrado');
             res.send('NOT_FOUND');
         }
     } catch (err) {
         console.log(err)
+        res.send('ERROR');
     }
 
 }));
 
 
-var server = app.listen(80, function () {
+var server = app.listen(88, function () {
     //app.use(cors())
     console.log("El servidor funciona en el puerto 80");
 });
