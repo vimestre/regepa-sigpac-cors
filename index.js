607778b8c368e7bb8b11671d712f6e192b533e7f
vimestre <vimestre@canamashnos.com>
2023-06-20 12:21:07 +0200

    añadido buscar parcela por id

--- index.js
+++ index.js
@@ -15,6 +15,28 @@ app.use(express.urlencoded({ extended: true }));
 
 
 
+app.get('/Coord/:lat/:lng', (async (req, res) => {
+    //https://sigpac.mapama.gob.es/vectorsdg/vector/parcela@3857/15.16377.20234.geojson
+
+    let lat = req.params.lat;
+    let lng = req.params.lng;
+
+    var ij = LatLng2IJ(lat, lng);
+    const urlSigPac = `https://sigpac.mapama.gob.es/vectorsdg/vector/parcela@3857/15.${ij.i}.${ij.j}.geojson`;
+
+    try {
+        const resp = await fetch(urlSigPac, {
+            method: 'GET'
+        });
+        const data = (await resp.json())
+        console.log(data);
+        res.send(200);
+
+    } catch (err) {
+        console.log(err);
+        res.send('ERROR')
+    }
+}))
 
 app.get('/Parcelas/:ProvinciaId/:MunicipioId/:PoligonoId', (async (req, res) => {
     const MunicipioId = req.params.MunicipioId;
@@ -100,6 +122,61 @@ app.get('/Regepa/:NIF', (async (req, res) => {
 
 }));
 
+
+var isPointInsidePolygon = function (punto, poligono) {
+    var x = punto.x, y = punto.y;
+
+    var vs = poligono[0];
+
+    var inside = false;
+    for (var i = 0, j = vs.length - 1; i < vs.length; j = i++) {
+        var xi = vs[i][0], yi = vs[i][1];
+        var xj = vs[j][0], yj = vs[j][1];
+
+
+        var intersect = ((yi > y) != (yj > y))
+            && (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
+
+
+        if (intersect) inside = !inside;
+    }
+
+    if (inside) {
+        for (var k = 1; k < poligono.length; k++) {
+
+            vs = poligono[k];
+
+            for (var i = 0, j = vs.length - 1; i < vs.length; j = i++) {
+                var xi = vs[i][0], yi = vs[i][1];
+                var xj = vs[j][0], yj = vs[j][1];
+
+                var intersect = ((yi > y) != (yj > y))
+                    && (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
+                if (intersect) {
+                    return false;
+                }
+            }
+        }
+    }
+
+    return inside;
+};
+
+var LatLng2XY = function (latlng) {
+    var y = Math.log(Math.tan(Math.PI / 4 + (latlng.lat * Math.PI) / 360)) * 6378137;
+    var x = (latlng.lng / 180) * 6378137 * Math.PI;
+    return { x: x, y: y }
+}
+
+var LatLng2IJ = function (lat, lng) {
+    var y = Math.log(Math.tan(Math.PI / 4 + (lat * Math.PI) / 360)) * 6378137;
+    var x = (lng / 180) * 6378137 * Math.PI;
+
+    var e = Math.pow(2, 15);
+    var i = Math.floor((x - (-20037508.3427892)) / ((20037508.3427892 - (-20037508.3427892)) / (e * 1)));
+    var j = Math.floor((y - (-20037508.3427892)) / ((20037508.3427892 - (-20037508.3427892)) / (e * 1)));
+    return { i: i, j: j };
+}
 
 var server = app.listen(88, function () {
     //app.use(cors())
